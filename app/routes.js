d36abc7ae287d1738a1f70a46a53167271030b93
Michael Kosler <michaelkosler@gmail.com>
2015-06-11 22:36:33 -0500

    Began work on steam api

--- app/routes.js
+++ app/routes.js
@@ -1,8 +1,18 @@
 var path = require('path');
+var passport = require('passport');
 
 module.exports = function (app) {
     // === frontend routes ===
     app.get('*', function (req, res) {
         res.sendFile(path.resolve('public/index.html'));
+    });
+
+    // === authentication routes ===
+    app.get('/auth/steam', passport.authenticate('steam'), function (req, res) {
+        // redirected to Steam
+    });
+
+    app.get('/auth/steam/return', passport.authenticate('steam'), function (req, res) {
+        res.send('Logged in!');
     });
 };
--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 // === modules ===
 var express = require('express');
+var passport = require('passport');
+var SteamStrategy = require('passport-steam').Strategy;
+var SECRETS = require('./.secrets.json');
 var app = express();
 
 // === configuration ===
 var port = process.env.PORT || 8080;
+
+passport.serializeUser(function (user, done) {
+});
+
+passport.deserializeUser(function (obj, done) {
+});
+
+passport.use(new SteamStrategy({
+    apiKey: SECRETS.STEAM_WEB_API_KEY,
+    returnURL: 'http://localhost:' + port + '/auth/steam/return',
+    realm: 'http://localhost:' + port + '/'
+}, function (identifier, profile, done) {
+}));
 
 app.use(express.static('public'));
 
