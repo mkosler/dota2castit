a15a82006d23a573b9d0a056bd8105b32dc46384
Michael Kosler <michaelkosler@gmail.com>
2015-06-19 07:01:26 -0500

    match/:id api call now returns both match info and steam profile info (for public steam accounts)

--- app/routes/dota-routes.js
+++ app/routes/dota-routes.js
@@ -4,21 +4,39 @@ var Dota2Api = require('../services/dota-api-service');
 var dotaService = new Dota2Api(SECRETS.STEAM_WEB_API_KEY);
 
 router.get('/match/:id', function (req, res) {
-    var match, players;
+    var match;
 
     dotaService.getMatchDetails({ match_id: req.params.id })
         .then(function (response) {
-            debugger;
-            match = JSON.parse(response);
+            match = response.result;
+
+            match.players.forEach(function (player) {
+                player.account_id64 = dotaService.convertIdTo64(player.account_id).toString();
+            });
+
+            var playerIds = match.players.map(function (player) {
+                return player.account_id64;
+            });
 
             return dotaService.getPlayerSummaries({
-                steamids: match.players.map(function (player) {
-                    return dotaService.convertIdTo64(player.account_id);
-                }).join(',')
+                steamids: playerIds.join(',')
             });
-        }).then(function (response) {
-            debugger;
-            players = response.players;
+        })
+        .then(function (response) {
+            response.response.players.forEach(function (p1) {
+                match.players.forEach(function (p2) {
+                    if (p1.steamid === p2.account_id64) {
+                        p2.steamaccount = p1;
+
+                        return;
+                    }
+                });
+            });
+
+            res.send(match);
+        })
+        .catch(function (reason) {
+            res.status(reason.statusCode).send(reason.error);
         });
 });
 
--- app/services/dota-api-service.js
+++ app/services/dota-api-service.js
@@ -1,97 +1,200 @@
 var Promise = require('bluebird');
-var request = Promise.promisify(require('request'));
-Promise.promisifyAll(request);
-var url = require('url');
+var request = require('request');
+var urljoin = require('url-join');
+var bignum = require('bignum');
 
 var Dota2Api = function (key) {
     this.key = key;
-    this._conversionMagicNumber = 76561197960265728;
+    this._conversionMagicNumber = bignum('76561197960265728');
 
     var baseUri = 'https://api.steampowered.com/';
-    this._matchBaseUri = url.resolve(baseUri, '/IDOTA2Match_570/');
-    this._econBaseUri = url.resolve(baseUri, '/IEconDOTA2_570/');
-    this._playerBaseUri = url.resolve(baseUri, '/ISteamUser/');
+    this._matchBaseUri = urljoin(baseUri, '/IDOTA2Match_570/');
+    this._econBaseUri = urljoin(baseUri, '/IEconDOTA2_570/');
+    this._playerBaseUri = urljoin(baseUri, '/ISteamUser/');
 };
 
 Dota2Api.prototype = {
-    applyApiKey: function (qs) {
-        qs.key = this.key;
-
-        return qs;
-    },
-
     convertIdTo64: function (id) {
-        return id + this._conversionMagicNumber;
+        var bigId = bignum(id);
+
+        return bigId.add(this._conversionMagicNumber);
     },
 
     convertIdTo32: function (id) {
-        return id - this._conversionMagicNumber;
+        var bigId = bignum(id);
+
+        return bigId.sub(this._conversionMagicNumber);
     },
 
+    call: function (uri, qs) {
+        qs.key = this.key;
+
+        return new Promise(function (resolve, reject) {
+            request({
+                uri: uri,
+                qs: qs
+            }, function (err, res, body) {
+                if (!err && res.statusCode === 200) {
+                    return resolve(JSON.parse(body));
+                }
+
+                reject({
+                    error: err,
+                    statusCode: res.statusCode
+                });
+            });
+        });
+    },
 
     // === API methods ===
     getMatchHistory: function (qs) {
-        return request({
-            uri: url.resolve(this._matchBaseUri, '/GetMatchHistory/v001/'),
-            qs: this.applyApiKey(qs)
-        });
+        return this.call(urljoin(this._matchBaseUri, '/GetMatchHistory/v001/'), qs);
+        //qs.key = this.key;
+
+        //return new Promise(function (resolve, reject) {
+            //request({
+                //uri: 'https://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v001/',
+                //qs: qs
+            //}, function (err, res, body) {
+                //if (!err && res.statusCode === 200) {
+                    //return resolve(JSON.parse(body));
+                //}
+
+                //reject({
+                    //error: err,
+                    //statusCode: res.statusCode
+                //});
+            //});
+        //});
     },
 
     getMatchDetails: function (qs) {
-        return request.getAsync({
-            uri: url.resolve(this._matchBaseUri, '/GetMatchDetails/v001/'),
-            qs: this.applyApiKey(qs)
-        });
+        return this.call(urljoin(this._matchBaseUri, '/GetMatchDetails/v001/'), qs);
     },
 
     getHeroes: function (qs) {
-        return request({
-            uri: url.resolve(this._econBaseUri, '/GetHeroes/v0001/'),
-            qs: this.applyApiKey(qs)
-        });
+        return this.call(urljoin(this._econBaseUri, '/GetHeroes/v0001/'), qs);
+        //qs.key = this.key;
+
+        //return new Promise(function (resolve, reject) {
+            //request({
+                //uri: 'https://api.steampowered.com/IEconDOTA2_570/GetHeroes/v0001/',
+                //qs: qs
+            //}, function (err, res, body) {
+                //if (!err && res.statusCode === 200) {
+                    //resolve(JSON.parse(body));
+                //}
+
+                //reject(err);
+            //});
+        //});
     },
 
     getPlayerSummaries: function (qs) {
-        return request({
-            uri: url.resolve(this._playerBaseUri, '/GetPlayerSummaries/v0002/'),
-            qs: this.applyApiKey(qs)
-        });
+        return this.call(urljoin(this._playerBaseUri, '/GetPlayerSummaries/v0002/'), qs);
+        //qs.key = this.key;
+
+        //return new Promise(function (resolve, reject) {
+            //request({
+                //uri: 'https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/',
+                //qs: qs
+            //}, function (err, res, body) {
+                //if (!err && res.statusCode === 200) {
+                    //resolve(JSON.parse(body));
+                //}
+
+                //reject(err);
+            //});
+        //});
     },
 
     getLeagueListing: function (qs) {
-        return request({
-            uri: url.resolve(this._matchBaseUri, '/GetLeagueListing/v0001/'),
-            qs: this.applyApiKey(qs)
-        });
+        return this.call(urljoin(this._matchBaseUri, '/GetLeagueListing/v0001/'), qs);
+        //qs.key = this.key;
+
+        //return new Promise(function (resolve, reject) {
+            //request({
+                //uri: 'https://api.steampowered.com/IDOTA2Match_570/GetLeagueListing/v0001/',
+                //qs: qs
+            //}, function (err, res, body) {
+                //if (!err && res.statusCode === 200) {
+                    //resolve(JSON.parse(body));
+                //}
+            //});
+        //});
     },
 
     getLiveLeagueGames: function (qs) {
-        return request({
-            uri: url.resolve(this._matchBaseUri, '/GetLiveLeagueGames/v0001/'),
-            qs: this.applyApiKey(qs)
-        });
+        return this.call(urljoin(this._matchBaseUri, '/GetLiveLeagueGames/v0001/'), qs);
+        //qs.key = this.key;
+
+        //return new Promise(function (resolve, reject) {
+            //request({
+                //uri: 'https://api.steampowered.com/IDOTA2Match_570/GetLiveLeagueGames/v0001/',
+                //qs: qs
+            //}, function (err, res, body) {
+                //if (!err && res.statusCode === 200) {
+                    //resolve(JSON.parse(body));
+                //}
+
+                //reject(err);
+            //});
+        //});
     },
 
     getMatchHistoryBySequenceNum: function (qs) {
-        return request({
-            uri: url.resolve(this._matchBaseUri, '/GetMatchHistoryBySequenceNum/v0001/'),
-            qs: this.applyApiKey(qs)
-        });
+        return this.call(urljoin(this._matchBaseUri, '/GetMatchHistoryBySequenceNum/v0001/'), qs);
+        //qs.key = this.key;
+
+        //return new Promise(function (resolve, reject) {
+            //request({
+                //uri: 'https://api.steampowered.com/IDOTA2Match_570/GetMatchHistoryBySequenceNum/v0001/',
+                //qs: qs
+            //}, function (err, res, body) {
+                //if (!err && res.statusCode === 200) {
+                    //resolve(JSON.parse(body));
+                //}
+
+                //reject(err);
+            //});
+        //});
     },
 
     getTeamInfoByTeamID: function (qs) {
-        return request({
-            uri: url.resolve(this._matchBaseUri, '/GetTeamInfoByTeamID/v001/'),
-            qs: this.applyApiKey(qs)
-        });
+        return this.call(urljoin(this._matchBaseUri, '/GetTeamInfoByTeamID/v001/'), qs);
+        //qs.key = this.key;
+
+        //return new Promise(function (resolve, reject) {
+            //request({
+                //uri: 'https://api.steampowered.com/IDOTA2Match_570/GetTeamInfoByTeamID/v001/',
+                //qs: qs
+            //}, function (err, res, body) {
+                //if (!err && res.statusCode === 200) {
+                    //resolve(JSON.parse(body));
+                //}
+
+                //reject(err);
+            //});
+        //});
     },
 
-    getGameItems: function (qs) {
-        return request({
-            uri: url.resolve(this._econBaseUri, '/GetGameItems/v001/'),
-            qs: this.applyApiKeys(qs)
-        });
-    }
+    //getGameItems: function (qs) {
+        //return this.call(urljoin(this._econBaseUri, '/Get
+        //qs.key = this.key;
+
+        //return new Promise(function (resolve, reject) {
+            //request({
+                //uri: 'https://api.steampowered.com/IEconDOTA2_570/GetTeamInfoByTeamID/v001/',
+                //qs: qs
+            //}, function (err, res, body) {
+                //if (!err && res.statusCode === 200) {
+                    //resolve(JSON.parse(body));
+                //}
+
+                //reject(err);
+            //});
+        //});
+    //}
 };
 
 module.exports = Dota2Api;
